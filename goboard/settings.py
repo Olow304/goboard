5a0aebeb32e4a7e438c7581d99b5f59426d770e4
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-06 13:21:13 -0500

    Update settings.py

--- goboard/settings.py
+++ goboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v^bo04y17xr=%z%lv$rf6qw1ik8ta))er8!5i!fdft#)o^9egz'
+SECRET_KEY = 'your secret key goes here...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
